6f5b5da858060738b8697f172d418a7f60f33c75
Alan Cox <etchedpixels@gmail.com>
2026-03-28 12:29:12 +0000

    ds1302: we have a method declared as bool that's asm - this is unwise
    Firstly our bool type varies by compiler and target, secondly the casting
    is then expected to occur in the asm function if needed, and we really don't
    want magic compiler knowledge there. The way we use it is such that a uint8_t
    is fine anyway and that's actually how the asm stuff treats it (or sometimes
    as unsigned which is fine too).

--- Kernel/dev/ds1302.h
+++ Kernel/dev/ds1302.h
@@ -36,7 +36,7 @@ uint_fast8_t uint8_from_bcd(uint_fast8_t value);
 void ds1302_set_clk(bool state) __fastcall;
 void ds1302_set_ce(bool state) __fastcall;
 void ds1302_set_data(bool state) __fastcall;
-bool ds1302_get_data(void);
+uint8_t ds1302_get_data(void);
 void ds1302_set_driven(bool state) __fastcall; /* 0=tristate for input, 1=driven for output */
 #endif
 
