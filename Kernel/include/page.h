e49b068492a645ed203328b513c2f51be866628c
Alan Cox <etchedpixels@gmail.com>
2026-04-25 14:09:11 +0100

    page: allow for the fact partitions can be over 65535 blocks

--- Kernel/include/page.h
+++ Kernel/include/page.h
@@ -4,4 +4,4 @@ extern int pageread(uint16_t dev, blkno_t blkno, usize_t nbytes,
                     paddr_t buf, page_t page);
 extern int pagewrite(uint16_t dev, blkno_t blkno, usize_t nbytes,
 		    paddr_t buf, page_t page);
-extern void pagefile_add_blocks(unsigned blocks);
+extern void pagefile_add_blocks(unsigned long blocks);
