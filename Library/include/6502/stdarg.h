af95fc885b712639bdb32f3cc3ee20f4dd8e2fcd
Alan Cox <etchedpixels@gmail.com>
2026-05-02 16:02:01 +0100

    6502: we stack bytes as bytes so adjust the new stdarg to be correct

--- Library/include/6502/stdarg.h
+++ Library/include/6502/stdarg.h
@@ -13,7 +13,7 @@
 
 typedef char* va_list;
 
-#define __vasz(x)		((sizeof(x)+sizeof(int)-1) & ~(sizeof(int) -1))
+#define __vasz(x)		sizeof(x)
 
 #define va_start(ap, parmN)	(ap = (va_list)&parmN + __vasz(parmN))
 #define va_arg(ap, type)	(*((type *)(void *)((ap += __vasz(type)) - __vasz(type))))
