7e01a3f5414502e8ec2b28883af91ec542213f64
Alan Cox <etchedpixels@gmail.com>
2026-05-09 17:53:06 +0100

    6502: update for registers and hogfather

--- Kernel/cpu-6502/image.mk
+++ Kernel/cpu-6502/image.mk
@@ -1,5 +1,8 @@
 tools/visualizefcc: tools/visualizefcc.c
 
+tools/hogfather: tools/hogfather.c
+
 fuzix.bin: target $(OBJS) tools/visualizefcc
 	+make -C platform/platform-$(TARGET) image
 	tools/visualizefcc <fuzix.map
+	tools/hogfather ../fuzix.map | sort -nr >../fuzix.hogs
