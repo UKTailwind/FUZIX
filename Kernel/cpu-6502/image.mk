687ea265fd1b57c4af8e4c4a4384d04a89889021
Alan Cox <etchedpixels@gmail.com>
2026-05-09 21:31:23 +0100

    6502: fix build of hogfather

--- Kernel/cpu-6502/image.mk
+++ Kernel/cpu-6502/image.mk
@@ -2,7 +2,7 @@ tools/visualizefcc: tools/visualizefcc.c
 
 tools/hogfather: tools/hogfather.c
 
-fuzix.bin: target $(OBJS) tools/visualizefcc
+fuzix.bin: target $(OBJS) tools/visualizefcc tools/hogfather
 	+make -C platform/platform-$(TARGET) image
 	tools/visualizefcc <fuzix.map
-	tools/hogfather ../fuzix.map | sort -nr >../fuzix.hogs
+	tools/hogfather ../fuzix.map | sort -nr >fuzix.hogs
