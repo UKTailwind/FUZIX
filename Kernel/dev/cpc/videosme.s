a25ae088515475e13d4c3fcdbf19d6e40d1b9e19
Antonio J. Casado Alías <dekkard.rick@gmail.com>
2026-07-06 12:30:33 +0200

    cpc/video: Fix bug in _plot_char video RAM and font rom mapping sequence, inverse attribute implementation and some speed optimizations.

--- Kernel/dev/cpc/videosme.s
+++ Kernel/dev/cpc/videosme.s
@@ -42,78 +42,46 @@ cpc_plot_char:
         add hl,hl
         ld bc, #_fontdata_8x8
         add hl, bc          ; hl points to first byte of char data
-        di
+        VIDEO_MAP
         ld bc, #0x7f8a ;RMR ->UROM disable LROM enable
         out (c),c
-        ld b,#6
-        ld a,(hl)
-        VIDEO_MAP
-        ld (de),a
+        ld bc,#0x800
+        ex af,af'
+        ld a,(_vtattr)
+        and #3          ;we only use the two lower bits, underline and inverted
+        or a            ;reset carry flag
+        rra             ;f' stores c=1 if inverted z=0 if underline
+        ex af,af'
+        ld a,#7
+        ex de,hl
 plot_char_line:
-        ld a,d
-        add a,#8             
-        ld  d,a              
-        inc hl
-        ld a,(hl)
-        ld (de),a
-        djnz plot_char_line
-        ld a,d
-        add a,#8             
-        ld  d,a              
-        inc hl
-        ld a,(hl)
+        ex af,af'
+        ld a,(de)
+        ld (hl),a
+        ex af,af'
+        add hl,bc
+        inc de
+        dec a
+        jr nz,plot_char_line
+        ex af,af'
+        ld a,(de)
         ld bc, #0x7f8e ;RMR ->UROM disable LROM disable
         out (c),c
-        ex af,af'
-        ld a,(_int_disabled)
-        or a
-        jr nz,cont_no_int_char
-        ei
-cont_no_int_char:
-        ex af,af'
-	; We do underline for now - not clear italic or bold are useful
+	; We do underline and inverted for now - not clear italic or bold are useful
 	; with the font we have.
-	ld bc,(_vtattr)	    ; c is vt attributes
-	bit 1,c		    ; underline ?
 	jr nz, last_ul
-plot_attr:
-        ld (de), a
+plot_ll:
+        jr nc,not_inverted
+        cpl
+not_inverted:    
+        ld (hl),a
+        ex af,af'
 	pop de
 	VIDEO_UNMAP
         ret
 last_ul:
 	ld a,#0xff
-	jr plot_attr
-
-	.if CPCVID_ONLY
-_clear_lines:
-	.endif
-cpc_clear_lines:
-        pop hl
-        pop de              ; E = line, D = count
-        push de
-        push hl
-	; This way we handle 0 correctly
-	inc d
-	jr nextline
-
-clear_next_line:
-        push de
-        ld d, #0            ; from the column #0
-        ld b, d             ; b = 0
-        ld c, #80           ; clear 80 cols
-        push bc
-        push de
-        call _clear_across
-        pop hl              ; clear stack
-        pop hl
-        pop de
-        inc e
-
-nextline:
-        dec d
-        jr nz, clear_next_line
-        ret
+	jr plot_ll
 
 	.if CPCVID_ONLY
 _clear_across:
@@ -130,39 +98,36 @@ cpc_clear_across:
 	ret z		    ; No work to do - bail out
 	push de
         call videopos       ; first pixel line of first character in DE
-        push de
-        pop hl              ; copy to hl
-        ld b,#8
+        ld h,d
+        ld l,e
+        ld b,c
+        ld c,#8
+        xor a
         VIDEO_MAP
 clear_line:
-        ld a, b
-        push af
         push bc
 clear_scanline:        ;see firmware SCR_NEXT_BYTE
-        xor a
         ld (de),a
         inc e
         jr nz,clear_scanline_cont
         inc d
         ld a,d
         and #7
-        jr nz,clear_scanline_cont
+        jr nz,clear_scanline_cont_xora
         ld a,d
         sub #8
         ld d,a
+clear_scanline_cont_xora:
+        xor a        
 clear_scanline_cont:
-        dec c
-        jr nz, clear_scanline
-        ex de, hl
-        ld a,d               
-        add a,#8             
-        ld  d,a              
+        djnz clear_scanline
+        ld bc,#0x800
+        add hl,bc
         pop bc
-        pop af
-        ld b,a
-        push de
-        pop hl
-        djnz clear_line
+        ld d,h
+        ld e,l
+        dec c
+        jr nz,clear_line
 	pop de
 	VIDEO_UNMAP
         ret
@@ -216,7 +181,7 @@ cpc_cursor_off:
 cursorpos:
         .dw 0
 
-
+;kernel and video maps share upper 16K ram bank at 0xc000-0xffff where common and kstack is placed 
 map_kernel:
         push bc
 	ld bc,#0x7fc2
@@ -238,7 +203,7 @@ map_video:
         ld a,(_outputtty)
         cp #4
         jr z,tty4
-	ld bc,#0x7fc1
+	ld c,#0xc1
 tty4:        
 	di
         out (c),c
@@ -248,6 +213,36 @@ tty4:
     
 
 .area _VIDEO
+
+	.if CPCVID_ONLY
+_clear_lines:
+	.endif
+cpc_clear_lines:
+        pop hl
+        pop de              ; E = line, D = count
+        push de
+        push hl
+	; This way we handle 0 correctly
+	inc d
+	jr nextline
+
+clear_next_line:
+        push de
+        ld d, #0            ; from the column #0
+        ld b, d             ; b = 0
+        ld c, #80           ; clear 80 cols
+        push bc
+        push de
+        call _clear_across
+        pop hl              ; clear stack
+        pop hl
+        pop de
+        inc e
+
+nextline:
+        dec d
+        jr nz, clear_next_line
+        ret
 
 	.if CPCVID_ONLY
 _scroll_up:
