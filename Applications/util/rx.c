cc28a7f5115f7f0e87ef49641a0bed1422867b90
Henrik Löfgren <henrik.lofgren@gmail.com>
2026-07-20 12:13:05 +0200

    Added support for baudrate setting and force overwrite + bugfixes

--- Applications/util/rx.c
+++ Applications/util/rx.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <termios.h>
 #include <fcntl.h>
+#include <errno.h>
 
 #define SOH         0x01 
 #define EOT         0x04
@@ -13,16 +15,53 @@
 static struct termios termsave;
 static struct termios termcur;
 static int ttyfd = -1;
-static uint8_t xmodem_buffer[128];
+static uint_fast8_t xmodem_buffer[128];
+static uint_fast8_t disp=0;
 FILE *receive_fp;
+
+static int baud[] = {
+    50,     /* B50 */
+    75,     /* B75 */
+    110,    /* B110 */
+    134,    /* B134 */
+    150,    /* B150 */
+    300,    /* B300 */
+    600,    /* B600 */
+    1200,   /* B1200 */
+    2400,   /* B2400 */
+    4800,   /* B4800 */
+    9600,   /* B9600 */
+    19200,  /* B19200 */
+    38400,  /* B38400 */
+    57600,  /* B57600 */
+    115200, /* B115200 */
+};
+  
+static speed_t speed[] = {
+    B50,
+    B75,
+    B110,
+    B134,
+    B150,
+    B300,
+    B600,
+    B1200,
+    B2400,
+    B4800,
+    B9600,
+    B19200,
+    B38400,
+    B57600,
+    B115200,
+};
 
 static void term_raw(int fd)  
 {
     tcgetattr(fd, &termsave);
     memcpy(&termcur, &termsave, sizeof(struct termios));
     cfmakeraw(&termcur);                                                
-    termcur.c_cc[VMIN] = 1;
-    termcur.c_cc[VTIME] = 0;
+    termcur.c_cc[VMIN] = 0;
+    termcur.c_cc[VTIME] = 10;
     tcsetattr(fd, TCSAFLUSH, &termcur);                                 
 }                                                                             
 
@@ -36,42 +75,42 @@ static void restore(int fd)
 }
 
 static int xmodem_receive(void) {
-    uint8_t block_cnt;
-    uint8_t block_exp = 1;
-    uint8_t pos = 0;
-    uint8_t inp;
-    uint8_t outp;
-    uint8_t checksum;
+    uint_fast8_t block_cnt;
+    uint_fast8_t block_exp = 1;
+    uint_fast8_t pos = 0;
+    uint_fast8_t inp;
+    uint_fast8_t outp;
+    uint_fast8_t checksum;
     
     outp = NAK;
     while(1) {
         write(ttyfd, &outp, 1);
         read(ttyfd, &inp, 1);
         if(inp == EOT) {
-            // Transmission done
+            /* Transmission done */
             outp = ACK;
             write(ttyfd, &outp, 1);
             return 0;
         }
-        if(inp == CAN) {
-            // Transmission cancelled
+        else if(inp == CAN) {
+            /* Transmission cancelled */
             return -1;
         }
-        if(inp == SOH) {
-            // Got header, get package
+        else if(inp == SOH) {
+            /* Got header, get package */
             outp = NAK;
             checksum = 0;
             read(ttyfd, &inp, 1);
             block_cnt = inp;
             read(ttyfd, &inp, 1);
             if((block_cnt == (inp ^0xFF)) && (block_cnt == block_exp)) {
-                // Get block, otherwise retry
+                /* Get block, otherwise retry */
                 for(pos=0; pos<128; pos++) {
                     read(ttyfd, &inp, 1);
                     xmodem_buffer[pos]=inp;
                     checksum += inp;
                 }
-                // Verify checksum
+                /* Verify checksum */
                 read(ttyfd, &inp, 1);
                 if(checksum == inp) {
                     outp = ACK;
@@ -79,46 +118,129 @@ static int xmodem_receive(void) {
                 }
                 block_exp++;
             }
-        }        
+        } else if(inp!=0)  {
+            /* Unexpected character - abort */
+            return -1;
+        }
+        if(disp) fputc('.',stderr);        
     }
 
 }
 
+static int parsespeed(char *str, speed_t *s) {
+    register int i;
+    register int b = atoi(str);
+    for(i =0; i<sizeof(baud) / sizeof(baud[0]); i++) {
+        if(baud[i] == b) {
+            *s = speed[i];
+            return 1;
+        }
+    }
+    return 0;
+}   
+    
+static void usage(void)
+{
+    fputs("rx - receive a file using X-modem\n", stderr);
+    fputs("Usage: rx [-t tty] [-b baudrate] [-f] filename\n", stderr);
+}
+
+
 int main(int argc, char *argv[]) 
 {
     const char *filename;
+    const char *ext_tty_filename;
     int ret;
-    if (argc == 2) {
-        /* rx filename */
-        ttyfd = STDIN_FILENO;
-        filename = argv[1];
+    int opt;
+    int flags;
+    int fd;
+    uint_fast8_t ext_tty=0;
+    uint_fast8_t overwrite=0;
+    speed_t speedval = 0;
 
+    while((opt = getopt(argc, argv, "t:b:f")) != -1) {
+        switch(opt) {
+            case 't':
+                /* Use specified TTY instead of STDIN */
+                ext_tty = 1;
+                ext_tty_filename = optarg;
+                break;
+            case 'b':
+                /* Parse baudrate */
+                if(!parsespeed(optarg, &speedval)) {
+                    fprintf(stderr, "Invalid baudrate: %s\n", optarg);
+                    return 1;
+                }
+                break;
+            case 'f':
+                /* Force overwrite of existing file */
+                overwrite = 1;
+                break;
+            default:
+                break;
+        }
+    }
+
+    if(optind < argc)
+       filename = argv[optind];
+    else {
+        usage();
+        return 1;
+    }
+        
+    /* Setup TTY */
+    if (!ext_tty) {
+        ttyfd = STDIN_FILENO;
+        disp = 0;
         if (!isatty(ttyfd)) {
             fprintf(stderr, "stdin is not a terminal\n");
             return 1;
         } 
-    } else if (argc == 4 && strcmp(argv[1], "-t") == 0) {
+    } else {
         /* open specified port */
-        ttyfd = open(argv[2], O_RDWR | O_NOCTTY);
+        ttyfd = open(ext_tty_filename, O_RDWR | O_NOCTTY);
         if (ttyfd < 0) {
             perror(argv[2]);
             return 1;
         }
+        disp = 1;
+    } 
 
-        filename = argv[3];
-    } else {
-        fputs("rx - receive a file using X-modem\n", stderr);
-        fputs("Usage: rx [-t tty] filename\n", stderr);
+    /* Open file to receive */
+    flags = O_WRONLY | O_CREAT;
+    
+    if(overwrite)
+        flags |=O_TRUNC;
+    else
+        flags |=O_EXCL;
+
+    fd = open(filename, flags, 0644);
+    receive_fp = fdopen(fd, "wb");
+    if(!receive_fp) {
+        perror(filename);
+        close(fd);
         return 1;
     }
-
+    
     fputs("Waiting for sender to initiate X-modem transfer\n",stderr);
-    receive_fp = fopen(filename, "w");
+    if(ttyfd == STDIN_FILENO)
+        fputs("Press any key to cancel\n",stderr);
+    
     term_raw(ttyfd);
-    ret = xmodem_receive();   
+    if(speedval > 0) {
+        if(cfsetospeed(&termcur, (speed_t)speedval) < 0 ||
+           tcsetattr(ttyfd, TCSAFLUSH, &termcur) < 0) {
+            
+            restore(ttyfd);
+            perror("baudrate");
+            exit(1);
+        }
+    }
+ 
+    ret = xmodem_receive();
     fclose(receive_fp);
     restore(ttyfd);
-    if(ret<0) fputs("X-modem transmission cancelled\n", stderr);
-    else fputs("X-modem transmission complete\n",stderr);
+    if(ret<0) fputs("Transfer cancelled\n", stderr);
+    else fputs("Transfer complete\n",stderr);
 }
 
