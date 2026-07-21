6726bae76d74f206bd28aab483a7987486f950c7
Henrik Löfgren <henrik.lofgren@gmail.com>
2026-07-21 19:27:25 +0200

    Added flush before starting reception

--- Applications/util/rx.c
+++ Applications/util/rx.c
@@ -268,7 +268,7 @@ int main(int argc, char *argv[])
         }
     }
     term_raw(ttyfd);
-
+    tcflush(ttyfd, TCIOFLUSH);
     ret = xmodem_receive();
     fclose(receive_fp);
     restore(ttyfd);
